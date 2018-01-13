3309eb78039f0979f0e4f8d7993bacbee65a94d9
vinhluong274 <vinhluong274@gmail.com>
2018-01-13 17:32:10 -0500

    completed problems 1-3

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -10,22 +10,21 @@
 
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
-
-from flask import Flask
+import requests
+from flask import Flask, request
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
+    return 'Welcome to SI 364!'
 
 
-if __name__ == '__main__':
-    app.run()
 
 
 ## [PROBLEM 2] - 250 points
 ## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', you should see something like the data shown in the included file sample_ratatouille_data.txt, which contains data about the animated movie Ratatouille. However, if you go to the url http://localhost:5000/movie/titanic, you should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl' for example, you should see data on the page that looks like this:
+
 
 # {
 #  "resultCount":0,
@@ -38,12 +37,41 @@ if __name__ == '__main__':
 ## Of course, you'll also need the requests library and knowledge of how to make a request to a REST API for data.
 
 ## Run the app locally (repeatedly) and try these URLs out!
+@app.route('/movie/<movie>')
+def movie(movie):
+    url = "https://itunes.apple.com/search?term=" + movie + "&entity=movie"
+    data = requests.get(url)
+    string = data.json()
+    return "<p>{}</p>".format(string)
+
+
 
 ## [PROBLEM 3] - 250 points
 
 ## Edit the above Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number.
 ## Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
+@app.route('/question')
+def question():
+    s = """<!DOCTYPE html>
+<html>
+<body>
+<form action="/result" method="GET">
+  Enter Your Favorite Number:<br>
+  <input type="text" name="number">
+  <br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>"""
+    return s
+
+
+@app.route('/result', methods = ["POST", "GET"])
+def result():
+    number = int(request.form["number"])
+    double = number * 2
+    return "<h3>Double your favorite number is {}</h3>".format(double)
 
 
 ## [PROBLEM 4] - 350 points
@@ -65,3 +93,28 @@ if __name__ == '__main__':
 # You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
 
 # Points will be assigned for each specification in the problem.
+@app.route('/problem4form',methods=["GET","POST"])
+def form():
+    formstring = """<br><br>
+    What is your favorite book?: <br>
+    <form action="./problem4form" method='POST'>
+<input type="text" name="book"> <br>
+<input type="submit" value="Submit">
+"""
+    if request.method == "POST":
+        book = request.form["book"]
+        url = "https://www.googleapis.com/books/v1/volumes?q=title:" + book
+        data = requests.get(url)
+        items = data.json()["items"]
+        string = items[1]['description']
+        # for i in items:
+        #     if "description" in i:
+        #         string = i["description"]
+        return formstring + string
+
+    else:
+        return formstring
+
+
+if __name__ == '__main__':
+    app.run()
